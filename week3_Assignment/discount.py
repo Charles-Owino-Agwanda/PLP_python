0194e8c9052786d2e682bdcea0a11816c87106b4
Charles-Owino-Agwanda <agwandacharlesowino@gmail.com>
2025-03-31 16:27:02 +0300

    discount calculator

--- week3_Assignment/discount.py
+++ week3_Assignment/discount.py
@@ -1,3 +1,11 @@
+# In the program a function named calculate_discount() is created.
+
+#The function takes original price and discount_percent as parameters
+
+#If discount is 20% or higher the discount is applied, else no discount
+
+# The final price is printed as the output. 
+
 def main():
     original_price = float(input("Enter the original price: $"))
     disc_percent = float(input("Enter the percent discount with no percent sign: "))
